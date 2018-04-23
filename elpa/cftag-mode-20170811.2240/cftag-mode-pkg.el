13768666bf32acc72ab6597c1565e2a979a4a55a
Sed Seyedi <sed@sunsed.com>
2018-04-23 07:40:59 -0700

    add cfml support

---
+++ elpa/cftag-mode-20170811.2240/cftag-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cftag-mode" "20170811.2240" "Emacs mode for editing tag-based CFML files" '((emacs "25")) :commit "86e77dcbb583191a3e755bdc29534f33d82bfc56" :url "https://github.com/am2605/cftag-mode")
